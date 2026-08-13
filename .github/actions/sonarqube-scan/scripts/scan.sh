9878674e88dd91d3a99cb462275f64d7a5cdaaeb
JAGT1806 <jagt1806@gmail.com>
2026-08-13 07:40:41 -0500

    feat: add new sonar action

--- .github/actions/sonarqube-scan/scripts/scan.sh
+++ .github/actions/sonarqube-scan/scripts/scan.sh
@@ -4,8 +4,32 @@ set -euo pipefail
 : "${SONAR_HOST_URL:?Environment variable SONAR_HOST_URL is required}"
 : "${SONAR_TOKEN:?Environment variable SONAR_TOKEN is required}"
 : "${PROJECT_KEY:?Environment variable PROJECT_KEY is required}"
-: "${STACK:?Environment variable STACK is required}":
-: "${BRANCH_NAME:?Environment variable BRANCH_NAME is required}"
+: "${STACK:?Environment variable STACK is required}"
+: "${ANALISYS_TYPE:?Environment variable ANALISYS_TYPE is required}"
+
+if [[ "$ANALISYS_TYPE" == "pull_request" ]]; then
+  : "${PR_KEY:?PR_KEY is required for pull_request analysis}"
+  : "${PR_BRANCH:?PR_BRANCH is required for pull_request analysis}"
+  : "${PR_BASE:?PR_BASE is required for pull_request analysis}"
+
+  CONTEXT_ARGS=(
+    "-Dsonar.pullrequest.key=$PR_KEY"
+    "-Dsonar.pullrequest.branch=$PR_BRANCH"
+    "-Dsonar.pullrequest.base=$PR_BASE"
+  )
+
+  echo "Running SonarQube scan for PR #$PR_KEY ($PR_BRANCH -> $PR_BASE) "
+else
+  : "${BRANCH_NAME:?BRANCH_NAME is required for branch analysis}"
+  CONTEXT_ARGS=("-Dsonar.branch.name=$BRANCH_NAME")
+  echo "Running SonarQube scan for branch: $BRANCH_NAME"
+fi
+
+
+QG_ARGS=()
+if [ "${QUALIY_GATE_WAIT:-false}" == "true" ]; then
+  QR_ARGS=("-Dsonar.qualitygate.wait=true")
+fi
 
 case $STACK in
   "java-maven")
@@ -16,7 +40,9 @@ case $STACK in
       -Dsonar.host.url="$SONAR_HOST_URL" \
       -Dsonar.login="$SONAR_TOKEN" \
       -Dsonar.branch.name="$BRANCH_NAME" \
-      -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml
+      -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/jacoco.xml \
+      "${CONTEXT_ARGS[@]}" \
+      "${QR_ARGS[@]}"
     ;;
   "node-npm")
     echo "Install sonar-scanner CLI..."
@@ -29,7 +55,9 @@ case $STACK in
       -Dsonar.branch.name="$BRANCH_NAME" \
       -Dsonar.sources="$SOURCES" \
       -Dsonar.javascript.lcov.reportPaths="$COVERAGE_PATH" \
-      -Dsonar.typescript.lcov.reportPaths="$COVERAGE_PATH"
+      -Dsonar.typescript.lcov.reportPaths="$COVERAGE_PATH" \
+      "${CONTEXT_ARGS[@]}" \
+      "${QR_ARGS[@]}"
     ;;
   *)
     echo "Unsupported stack: $STACK"
